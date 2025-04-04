e078ca4d556e0432694b9f0c27c511580a732d74
Renan William Alves de Paula <renan@4success.com.br>
2025-04-04 15:32:14 -0300

    Update @tunnelhub/sdk to v2.7.0 and refactor integration logic
    Upgraded @tunnelhub/sdk to version 2.7.0 to include the latest enhancements and features. Refactored `Integration` class to remove unused `parameters` and `systems` properties, simplifying the implementation and ensuring alignment with the updated SDK. This change improves maintainability and adheres to the updated SDK structure.

--- src/core/integration.ts
+++ src/core/integration.ts
@@ -1,21 +1,10 @@
-import {
-  AutomatedIntegrationParameters,
-  IntegrationMessageReturn,
-  Metadata,
-  NoDeltaIntegrationFlow,
-  TunnelHubSystem,
-} from '@tunnelhub/sdk';
+import { IntegrationMessageReturn, Metadata, NoDeltaIntegrationFlow } from '@tunnelhub/sdk';
 import { IntegrationModel } from '../types/integration';
 import metadata from '../metadata';
 
 export default class Integration extends NoDeltaIntegrationFlow<IntegrationModel> {
-  private readonly parameters: AutomatedIntegrationParameters;
-  private readonly systems: TunnelHubSystem[];
-
   constructor(event: any, context: any) {
     super(event, context);
-    this.systems = event.systems ?? [];
-    this.parameters = event.parameters ?? {};
     /**
      * It is mandatory to have the constructor call the super with the event of the main handler.
      * You can get the systems configured in automation and save them in a class attribute for further use.
